a1dc3545cfcbaddc6e3e9ba4904490513ae5f885
nesmussa <emileofnm@gmail.com>
2025-03-21 14:46:19 +0300

    Nav bar for Kitchen_menu_bar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Kitchen_manager from './pages/Kitchen_manager'
+
+function App() {
+  return (
+    <div>
+        <Kitchen_manager/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Kitchen_manager.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Kitchen_menu_bar from '../components/Kitchen_menu_bar/Kitchen_menu_bar'
+
+function Kitchen_manager() {
+  return (
+    <div>
+        <Kitchen_menu_bar/>
+    </div>
+  )
+}
+
+export default Kitchen_manager
