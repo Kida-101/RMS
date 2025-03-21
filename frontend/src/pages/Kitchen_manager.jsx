e9ef34214e857e89c2026c6ec9a061197e8dd372
nesmussa <emileofnm@gmail.com>
2025-03-21 15:20:22 +0300

    Updated logic in Kitchen_manager component

--- frontend/src/pages/Kitchen_manager.jsx
+++ frontend/src/pages/Kitchen_manager.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import './styles/Kitchen_manager.css'
 import Kitchen_menu_bar from '../components/Kitchen_menu_bar/Kitchen_menu_bar'
 
 function Kitchen_manager() {
   return (
-    <div>
-        <Kitchen_menu_bar/>
+    <div className='Kitchen_manager_continer'>
+        <div className='Kitchen_menu_bar'><Kitchen_menu_bar/></div>
+        <div className='main'>
+          <h1>Kitchen Manager other components</h1>
+        </div>
     </div>
   )
 }
