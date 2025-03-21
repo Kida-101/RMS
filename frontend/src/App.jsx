b8e82541c98d3eab1937e07ac21d8e21748b30d1
SILAMLAK DESALEGN <siluk7068@gmail.com>
2025-03-21 04:29:51 -0700

    original

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Reservation from "./pages/reservation";
+
+const App = () => {
+  return (
+    <div>
+      <Reservation />
+    </div>
+  );
+};
+
+export default App;
