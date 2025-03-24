7e66ae0952a1ab4082c1c6f1c9aee587149e410d
SILAMLAK DESALEGN <siluk7068@gmail.com>
2025-03-24 05:23:44 -0700

    updated reservation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,52 +1,43 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
+// import Routing from "./pages/router/router";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import KitchenManager from "./pages/Kitchen_manager";
 import StoreKeeper from "./pages/StoreKeeper";
 import ManagerDashboard from "./pages/managerDashboard";
 import React from "react";
-import Routing from "./pages/router/router";
 const App = () => {
   return (
-    <BrowserRouter>
-      <Router>
-        <nav className="bg-gray-800 p-4">
-          <ul className="flex space-x-6 text-white">
-            <li>
-              <Link to="/" className="hover:text-gray-400">
-                Reservation
-              </Link>
-            </li>
-            <li>
-              <Link to="/manager" className="hover:text-gray-400">
-                Manager Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link to="/storeKepper" className="hover:text-gray-400">
-                Store Keeper
-              </Link>
-            </li>
-            <li>
-              <Link to="/kitchen" className="hover:text-gray-400">
-                Kitchen Manager
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/" element={<Routing />} />
-          <Route path="/manager" element={<ManagerDashboard />} />
-          <Route path="/storeKepper" element={<StoreKeeper />} />
-          <Route path="/kitchen" element={<KitchenManager />} />
-        </Routes>
-      </Router>
-    </BrowserRouter>
+    <Router>
+      <nav className="bg-gray-800 p-4">
+        <ul className="flex space-x-6 text-white">
+          <li>
+            <Link to="/" className="hover:text-gray-400">
+              Reservation
+            </Link>
+          </li>
+          <li>
+            <Link to="/manager" className="hover:text-gray-400">
+              Manager Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/storeKepper" className="hover:text-gray-400">
+              Store Keeper
+            </Link>
+          </li>
+          <li>
+            <Link to="/kitchen" className="hover:text-gray-400">
+              Kitchen Manager
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        {/* <Route path="/" element={<Routing />} />{" "} */}
+        <Route path="/manager" element={<ManagerDashboard />} />
+        <Route path="/storeKepper" element={<StoreKeeper />} />
+        <Route path="/kitchen" element={<KitchenManager />} />
+      </Routes>
+    </Router>
   );
 };
 
