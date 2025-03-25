0a8de12a39b9f91c46c2b6e50104edd7e92340af
SILAMLAK DESALEGN <siluk7068@gmail.com>
2025-03-25 16:18:23 -0700

    changing to tailwind css

--- frontend/src/components/order_reservation/home/home.jsx
+++ frontend/src/components/order_reservation/home/home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import "./home.css";
+
 const Home = () => {
   return (
-    <div className="background">
-      <div className="bg-image">
-        <img src="../../../src/assets/food-bg/3.png" alt="banner-background" />
+    <div className="relative flex justify-center items-center h-screen text-center overflow-hidden">
+      <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1] opacity-80">
+        <img
+          src="../../../src/assets/food-bg/3.png"
+          alt="banner-background"
+          className="max-w-[80vw] h-auto drop-shadow-[0px_4px_10px_rgba(0,0,0,0.8)]"
+        />
       </div>
       <div>
-        <h1 className="banner-txt">Delicious Meals, Just a Click Away!</h1>
-        {/* <p className="banner-slogan">
-          Order your favorite dishes fresh & fast delivered right to your table
-          or door!
-        </p> */}
+        <h1 className="flex text-center text-[70px] italic text-orange-500 font-extrabold mt-[180px] relative z-[1] mb-[5px] shadow-[2px_2px_8px_rgba(0,0,0,0.5)] font-[Rock_Salt,Caveat_Brush,Dancing_Script,cursive]">
+          Delicious Meals, Just a Click Away!
+        </h1>
       </div>
     </div>
   );
--- frontend/src/components/order_reservation/router/router.jsx
+++ frontend/src/components/order_reservation/router/router.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 import Reservation from "../../../pages/reservation";
 import Menu from "../menu/menu";
-import "./router.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../home/home";
 import TableBooking from "../table/table";
 
 const Routing = () => {
   return (
-    <>
-      <BrowserRouter>
-        <div className="app-container">
-          <div className="sidebar">
-            <Reservation />
-          </div>
-          <div className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/menu" element={<Menu />} />
-              <Route path="/table" element={<TableBooking />} />
-            </Routes>
-          </div>
+    <BrowserRouter>
+      <div className="flex min-h-[300vh] w-full h-[100px] bg-[url('../../../src/assets/food-bg/44.jfif')] bg-cover bg-center bg-black/80 bg-blend-darken font-[Poppins]">
+        <div className="flex-1 min-w-[100px] h-screen">
+          <Reservation />
         </div>
-      </BrowserRouter>
-    </>
+        <div className="flex-2 p-[25px] mb-[30px]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/table" element={<TableBooking />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
