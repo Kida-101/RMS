dd63ca16f7851c165fee74e307750aa1e407efbb
SILAMLAK DESALEGN <siluk7068@gmail.com>
2025-03-28 15:51:37 -0700

    upgrades with Reception page

--- frontend/src/pages/reservation.jsx
+++ frontend/src/pages/reservation.jsx
@@ -36,14 +36,14 @@ const ReservationForm = ({
   };
   return (
     <div className="max-w-[400px] w-full m-[25px_30px] p-[25px] max-w-full sm:max-w-[400px] bg-[#dfdfdf] bg-cover rounded-[10px] shadow-[0px_4px_8px_rgba(0,0,0,0.7)]">
-      <div className="mb-4">
-        <h1 className="text-[#45a049] text-center text-2xl md:text-3xl font-extrabold">
-          Order form&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <button onClick={() => activecomponent("/")} className="nav-link">
-            <i className="fa-solid fa-house text-black pr-2" />
-          </button>
-        </h1>
-      </div>
+     <div className="mb-4 flex flex-row justify-between items-center">
+  <h1 className="text-[#45a049] text-2xl md:text-3xl font-extrabold">
+    Order form
+  </h1>
+  <button onClick={() => activecomponent("/")}>
+    <i className="fa-solid fa-house text-[#45a049] pr-2 text-[20px]" />
+  </button>
+</div>
       <div className="separator"></div>
       <form className="form-reservation flex flex-col" onSubmit={handleSubmit}>
         <div className="full-name mb-4">
