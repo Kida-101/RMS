afeb4673fe92130479f7ac5183262affbfe0b5e9
nattyemu <natnaelbayut@gmail.com>
2025-03-29 12:44:31 +0300

    RequestItem page implemented in chef task. AddStock page added in stockkeeper task. and for those implementation i add  react-hook-form dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,51 @@
 import React from "react";
 import Routing from "./components/order_reservation/router/router.jsx";
-// import Chef from "./pages/Chef";
-// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import KitchenManager from "./pages/Kitchen_manager";
-// import StoreKeeper from "./pages/StoreKeeper";
-// import ManagerDashboard from "./pages/managerDashboard";
+import Chef from "./pages/Chef";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import KitchenManager from "./pages/Kitchen_manager";
+import StoreKeeper from "./pages/StoreKeeper";
+import ManagerDashboard from "./pages/managerDashboard";
 
 const App = () => {
   return (
-    // <Router>
-    //   <nav className="bg-gray-800 p-4">
-    //     <ul className="flex space-x-6 text-white">
-    //       <li>
-    //         <Link to="/" className="hover:text-gray-400">
-    //           Reservation
-    //         </Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/manager" className="hover:text-gray-400">
-    //           Manager Dashboard
-    //         </Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/storeKepper" className="hover:text-gray-400">
-    //           Store Keeper
-    //         </Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/kitchen" className="hover:text-gray-400">
-    //           Kitchen Manager
-    //         </Link>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    //   <Routes>
-    //     {/* <Route path="/" element={<Routing />} />{" "} */}
-    //     <Route path="/manager" element={<ManagerDashboard />} />
-    //     <Route path="/storeKepper" element={<StoreKeeper />} />
-    //     <Route path="/kitchen" element={<KitchenManager />} />
-    //   </Routes>
-    // </Router>
-    <div>
-      <Routing />
-    </div>
+    <Router>
+      <nav className="bg-gray-800 p-4">
+        <ul className="flex space-x-6 text-white">
+          <li>
+            <Link to="/" className="hover:text-gray-400">
+              Reservation
+            </Link>
+          </li>
+          <li>
+            <Link to="/manager" className="hover:text-gray-400">
+              Manager Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/storeKepper" className="hover:text-gray-400">
+              Store Keeper
+            </Link>
+          </li>
+          <li>
+            <Link to="/kitchen" className="hover:text-gray-400">
+              Kitchen Manager
+            </Link>
+          </li>
+          <li>
+            <Link to="/chef" className="hover:text-gray-400">
+              Chef
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        {/* <Route path="/" element={<Routing />} />{" "} */}
+        <Route path="/manager" element={<ManagerDashboard />} />
+        <Route path="/storeKepper" element={<StoreKeeper />} />
+        <Route path="/kitchen" element={<KitchenManager />} />
+        <Route path="/chef" element={<Chef />} />
+      </Routes>
+    </Router>
   );
 };
 
