20d5010386e2fc2c8fdbd81ce02a7d60f2145cc7
nattyemu <natnaelbayut@gmail.com>
2025-04-10 12:52:23 +0300

    use import instade of require

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const pg = require('pg');
-const cors = require('cors');
-const port = process.env.PORT; // Default to 4000 if PORT is not defined in .env
+import dotenv from "dotenv";
+dotenv.config();
 
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+const app = express();
+const port = process.env.PORT; // Default to 4000 if PORT is not defined in .env
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World! Your server is running on the configured port.');
+app.get("/", (req, res) => {
+  res.send("Hello, World! Your server is running on the configured port.");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
