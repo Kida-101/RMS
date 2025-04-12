bced39826c2cbc83dc835aeef09f73b631a8a085
nesmussa <emileofnm@gmail.com>
2025-04-12 10:47:54 +0300

    make Casher_payment_receive back with front end

--- backend/components/Casher/Casher.js
+++ backend/components/Casher/Casher.js
@@ -6,5 +6,6 @@ app.use('/dashboard',dashboard)
 import paid_bill from'./compinents/Casher_paid_bill.js';
 app.use('/paid_bill',paid_bill)
 
-
+import Casher_payment_receive from'./compinents/Casher_payment_receive.js';
+app.use('/casher_payment_receive',Casher_payment_receive)
 export default app;
