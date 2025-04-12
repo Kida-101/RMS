e5f80a081881ca3581c9099bb3d38972e85a1ed2
nattyemu <natnaelbayut@gmail.com>
2025-04-12 10:30:18 +0300

    supplier api implmentation by using zod for validation

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import pg from "pg";
+import appRouter from "./src/routers/index.js";
 const app = express();
 const port = process.env.PORT; // Default to 4000 if PORT is not defined in .env
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+app.use("/api", appRouter);
 app.get("/", (req, res) => {
   res.send("Hello, World! Your server is running on the configured port.");
 });
